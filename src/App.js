8e0f275f66bb2103549ffba3037948e55c70289a
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-31 08:09:29 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(props) {
+  return (
+    <main className='App' >
+      {/* content goes here */}
+      {props.STORE}
+    </main>
+  );
+}
+
+export default App;
