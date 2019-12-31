988b63ade453f04fbe4f8d42dd5fc5cc97dc9f9a
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-31 09:02:27 -0800

    added components - App, List and Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import List from './List.js';
 
-function App(props) {
-  return (
-    <main className='App' >
-      {/* content goes here */}
-      {props.STORE}
-    </main>
-  );
+class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
+
+  render(){
+    const { store } = this.props
+    return (
+      <main className='App' >
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
